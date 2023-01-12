17d5479e1a00999aa61fb0a2cba50d8fcbf2d97f
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2023-01-12 17:43:16 -0300

    pixel

--- Voltz/src/components/router-head/router-head.tsx
+++ Voltz/src/components/router-head/router-head.tsx
@@ -12,7 +12,23 @@ export const RouterHead = component$(() => {
   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
       })(window, document, "clarity", "script", "f635ucsahn");`
 
+  // < !--Meta Pixel Code -->
+  const pixel = ` !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '510661534225018');
+    fbq('track', 'PageView');`
 
+  const noscript = `<img height="1" width="1" style="display:none"
+      src="https://www.facebook.com/tr?id=510661534225018&ev=PageView&noscript=1"
+    />`
+
+  // <!--End Meta Pixel Code-- >
   const head = useDocumentHead();
   const loc = useLocation();
 
@@ -36,6 +52,8 @@ export const RouterHead = component$(() => {
         <style {...s.props} dangerouslySetInnerHTML={s.style} />
       ))}
       <script > {clarity} </script>
+      <script> {pixel} </script>
+      <noscript>{noscript}</noscript>
     </>
   );
 });
