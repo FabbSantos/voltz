24d05f07afc79fab53bbb69666ebd7ba3864ec62
Fabrício Bahiense Santos <fabriciobs2000@gmail.com>
2023-01-11 23:48:33 -0300

    clarity added

--- Voltz/src/components/router-head/router-head.tsx
+++ Voltz/src/components/router-head/router-head.tsx
@@ -5,6 +5,14 @@ import { useDocumentHead, useLocation } from '@builder.io/qwik-city';
  * The RouterHead component is placed inside of the document `<head>` element.
  */
 export const RouterHead = component$(() => {
+
+  const clarity = `(function(c,l,a,r,i,t,y){
+    c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) }};
+  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "f635ucsahn");`
+
+
   const head = useDocumentHead();
   const loc = useLocation();
 
@@ -27,6 +35,7 @@ export const RouterHead = component$(() => {
       {head.styles.map((s) => (
         <style {...s.props} dangerouslySetInnerHTML={s.style} />
       ))}
+      <script > {clarity} </script>
     </>
   );
 });
